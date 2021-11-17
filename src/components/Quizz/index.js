91d52d9f6bb28efa66787a3e726a18c195bae985
marie-laure Debeaune <ml.debeaune@gmail.com>
2021-11-17 21:15:19 +0100

    update

--- src/components/Quizz/index.js
+++ src/components/Quizz/index.js
@@ -1,14 +1,37 @@
 import React, {Component} from 'react'
+import {QuizMarvel} from '../quizMarvel/'
+import Levels from '../Levels'
+import ProgressBar from '../ProgressBar'
 
 class Quizz extends Component {
+
+    state = {
+        levelNames: ["debutant","confirme","expert"],
+        quizLevel:0
+    }
+
+    loadQuestions = level => {
+        console.log(level);
+    }
+
+    componentDidMount() {
+        this.loadQuestions(this.state.levelNames[this.state.quizLevel])
+    }
 
     render() {
 
         const {pseudo} = this.props.userData;
 
         return(
-            <div>
-                <h2>Pseudo: {pseudo}</h2>
+            <div> 
+                <Levels />
+                <ProgressBar />
+                <h2>Notre question Quizz</h2>
+                <p className="answerQuestions">Question 1</p>
+                <p className="answerQuestions">Question 2</p>
+                <p className="answerQuestions">Question 3</p>
+                <p className="answerQuestions">Question 4</p>
+                <button className="btnSubmit">Suivant</button>
             </div>
         )
     }      
