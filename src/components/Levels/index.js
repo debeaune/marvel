c33c4cea7034586df02cc4aa728df52dad28614b
marie-laure Debeaune <ml.debeaune@gmail.com>
2021-12-10 21:47:38 +0100

    update react-stepper-horizontal

--- src/components/Levels/index.js
+++ src/components/Levels/index.js
@@ -6,24 +6,29 @@ const Levels = ({levelNames,quizLevel}) => {
     const [levels, setLevels] = useState([]);
 
     useEffect(()=>{
-        const quizSteps = levelNames.map( level => ({title:level}));
+        const quizSteps = levelNames.map( level => ({title:level.toUpperCase()}));
         setLevels(quizSteps);
         
     }, [levelNames]);
 
     return (
-        <div className="levelsContainer">
-            <div>
-                <Stepper steps={[
-                 { label: 'Débutant' },
-                 { label: 'Confirmé' }, 
-                 { label: 'Expert' }
-                 ]
-                 }
-                 activeStep={1}/>
-            </div>       
+        <div className="levelsContainer" style={{background:'transparent'}}>
+            <Stepper 
+                steps={levels}
+                 activeStep={quizLevel}
+                 circleTop={0}
+                 activeTitleColor={'#d31017'}
+                 activeColor={'#d31017'}
+                 completeTitleColor={'#E0E0E0'}
+                 defaultTitleColor={'#E0E0E0'}
+                 completeColor={'#E0E0E0'}
+                 completeBarColor={'#E0E0E0'}
+                 barStyle={'dashed'}
+                 size={45}
+                 circleFontSize={20}
+            />      
         </div>
     )
 }
 
-export default Levels;
+export default React.memo(Levels);
