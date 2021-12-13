2e93eebb7ee4b6e55e36a799a8e1fca30538b738
marie-laure Debeaune <ml.debeaune@gmail.com>
2021-12-13 21:31:41 +0100

    update modal and localStorage

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -2,6 +2,7 @@ import React, {Fragment,useEffect,useState} from 'react'
 import {GiTrophyCup} from 'react-icons/gi';
 import Loader from '../Loader'
 import Modal from '../Modal'
+import axios from 'axios'
 
 const QuizOver = React.forwardRef((props,ref) => {
 
@@ -15,21 +16,62 @@ const QuizOver = React.forwardRef((props,ref) => {
     }=props;
 
     const API_PUBLIC_KEY = process.env.REACT_APP_MARVEL_API_KEY;
-    const hash='de678a2f3b91a9c703b78b04c3c8ef96';
+    const hash='d772d8d13faca10b36ab6fb9d312d3f6';
 
     const [asked,setAsked] = useState([]);
     const [openModal, setOpenModal] = useState(false);
+    const [characterInfos, setCharacterInfos]= useState([]);
+    const [loading, setLoading]= useState(true);
 
     useEffect(() => {
         setAsked(ref.current)
+
+        if(localStorage.getItem('marvelStorageDate')){
+            const date=localStorage.getItem('marvelStorageDate');
+            checkDataAge(date);
+        }
     },[ref])
+
+    const checkDataAge = (date) => {
+        const today= Date.now();
+        const timeDifference = today - date;
+
+        const daysDifference = timeDifference / (1000 * 3600 * 24);
+
+        if(daysDifference >=15 ){
+            localStorage.clear();
+            localStorage.setItem('marvelStorageDate', Date.now());
+        }
+    }
 
     const showModal = id => {
         setOpenModal(true);
+
+        if(localStorage.getItem(id)) {
+            setCharacterInfos(JSON.parse(localStorage.getItem(id)));
+            setLoading(false);
+        }
+        else {
+            axios
+            .get(`https://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=${API_PUBLIC_KEY}&hash=${hash}`)
+            .then(response => { 
+                setCharacterInfos(response.data);
+                setLoading(false);
+    
+                localStorage.setItem(id,JSON.stringify(response.data));
+                if(!localStorage.getItem('marvelStorageDate')){
+                    localStorage.setItem('marvelStorageDate', Date.now());
+                }
+                
+            })
+            .catch( err => console.log(err));
+        }
+       
     }
 
     const hideModal = () => {
         setOpenModal(false);
+        setLoading(true);
     }
 
     const averageGrade = maxQuestions /2;
@@ -120,6 +162,34 @@ const QuizOver = React.forwardRef((props,ref) => {
             </td>
         </tr>
     )
+
+    const resultInModal = !loading ? 
+    (
+        <Fragment>
+            <div className="modalHeader">  
+                <h2>{characterInfos.data.results[0].name}</h2> 
+            </div>
+            <div className="modalBody">
+                <h3>Titre 2</h3>
+            </div>
+            <div className="modalFooter">
+                <button className="modalBtn">Fermer</button>
+            </div>
+        </Fragment>
+        
+    )
+    :
+    (
+        <Fragment>
+            <div className="modalHeader">  
+                <h2>Réponse de Marvel ...</h2> 
+            </div>
+            <div className="modalBody">
+                <Loader />
+            </div>
+        </Fragment>
+       
+    )
        
     return(
         <Fragment>
@@ -146,15 +216,7 @@ const QuizOver = React.forwardRef((props,ref) => {
             </div>
 
             <Modal showModal={openModal} hideModal={hideModal} >
-                <div className="modalHeader">  
-                    <h2>Titre</h2> 
-                </div>
-                <div className="modalBody">
-                    <h3>Titre 2</h3>
-                </div>
-                <div className="modalFooter">
-                    <button className="modalBtn">Fermer</button>
-                </div>
+                {resultInModal}
             </Modal>
 
         </Fragment>        
